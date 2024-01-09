aa55c4772049698f0015e90ea64ac7f3cbe62083
Nobil Gautam <nobilgautam007@gmail.com>
2024-01-09 22:57:11 +0530

    how it works cta added

--- src/pages/HowItWorks.js
+++ src/pages/HowItWorks.js
@@ -1,6 +1,7 @@
 import HowItWorksHeader from "../components/HowItWorksHeader";
 import HowItWorksTimeline from "../components/HowItWorksTimeline";
 import HowItWorksStories from "../components/HowItWorksStories";
+import HowItWorksGetStartedCTA from "../components/HowItWorksGetStartedCTA";
 
 const HowItWorks = () => {
     return(
@@ -8,6 +9,7 @@ const HowItWorks = () => {
             <HowItWorksHeader />
             <HowItWorksTimeline />
             {/* <HowItWorksStories /> */}
+            <HowItWorksGetStartedCTA />
         </>
     )
 }
