6e2e1e8c34f817ed96b673fd436002900c64750b
Hirelane <nobilgautam007@gmail.com>
2024-01-12 15:38:19 +0530

    content changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -19,11 +19,11 @@ const Header = () => (
             </h2>
             <h1 className=' text-[6rem] font-bold text-[#F13F25]'>TRIUMPH.</h1>
             <div className='w-[80%] flex flex-col gap-2'>
-                <p className='font-light'>Unlock unparalleled expertise with our curated pool of top-tier professionals ready to elevate your team.</p>
-                <p className='font-light'>Streamline your recruitment process for swift onboarding, ensuring your team is up and running in no time.</p>
-                <p className='font-light'>Maximize your budget without compromising quality with our our cost-effective solutions.</p>
+                <p className='font-light text-[1.2rem]'>Unlock unparalleled expertise with our curated pool of top-tier talent</p>
+                <p className='font-light text-[1.2rem]'>Streamline your recruitment as we ensure your team is up and running in no time</p>
+                <p className='font-light text-[1.2rem]'>Save on your budget with our cost-effective solutions</p>
             </div>
-            <Link to='/howItWorks'><Button variant='contained' endIcon={<SendIcon />} className='w-[35%]' 
+            <Link to='/howItWorks'><Button variant='contained' endIcon={<SendIcon />} className='w-[35%] py-4' 
             sx={{
                 my: 5,
                 backgroundColor: '#F13F25',
